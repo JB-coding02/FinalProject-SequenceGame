ade3842986230ad29e80b6f2d5ea795bed69e0a8
JB-coding02! <justinburns2008@gmail.com>
2026-01-27 16:14:29 -0800

    Added Database schema to solution explorer for easier modification in the future.

--- Final Project - Sequence Game/GameBoard.cs
+++ Final Project - Sequence Game/GameBoard.cs
@@ -5,56 +5,56 @@ using System.Data;
 using System.Drawing;
 using System.Text;
 using System.Windows.Forms;
+using Microsoft.Data.SqlClient;
 
-namespace Final_Project___Sequence_Game
+namespace Final_Project___Sequence_Game;
+
+public partial class GameBoard : Form
 {
-    public partial class GameBoard : Form
+    public GameBoard()
     {
-        public GameBoard()
-        {
-            InitializeComponent();
-        }
+        InitializeComponent();
+    }
 
-        public string[,] CreateGrid()
-        {
-            string[,] grid = new string[10, 10];
-            return grid;
-        }
+    public string[,] CreateGrid()
+    {
+        string[,] grid = new string[10, 10];
+        return grid;
+    }
 
-        public string[,] GetGrid()
-        {
-            string[,] gameGrid = CreateGrid();
-            return gameGrid;
-        }
+    public string[,] GetGrid()
+    {
+        string[,] gameGrid = CreateGrid();
+        return gameGrid;
+    }
 
-        public void SetGridValues()
+    public void SetGridValues()
+    {
+        string[,] grid = GetGrid();
+        for (int row = 0; row < 10; row++)
         {
-            string[,] grid = GetGrid();
-            for (int row = 0; row < 10; row++)
+            for (int col = 0; col < 10; col++)
             {
-                for (int col = 0; col < 10; col++)
-                {
-                    grid = GetCardArray();
-                }
+                grid = GetCardArray();
             }
         }
+    }
 
-        public string[,] GetCardArray()
+    public string[,] GetCardArray()
+    {
+        string[,] cardArray = new string[10, 10]
         {
-            string[,] cardArray = new string[10, 10]
-            {
-            { "FREE", "2S", "3S", "4S", "5S", "6S", "7S", "8S", "9S", "FREE" },
-            { "6C", "5C", "4C", "3C", "2C", "AH", "KH", "QH", "10H", "10S" },
-            { "7C", "AS", "2D", "3D", "4D", "5D", "6D", "7D", "9H", "QS" },
-            { "8C", "KS", "6C", "5C", "4C", "3C", "2C", "8D", "8H", "KS" },
-            { "9C", "QS", "7C", "6H", "5H", "4H", "AH", "9D", "7H", "AS" },
-            { "10C", "10S", "8C", "7H", "2H", "3H", "KH", "10D", "6H", "2D" },
-            { "QC", "9S", "9C", "8H", "9H", "10H", "QH", "QD", "5H", "3D" },
-            { "KC", "8S", "10C", "QC", "KC", "AC", "AD", "KD", "4H", "4D" },
-            {  "AC", "7S", "6S", "5S", "4S", "3S", "2S", "2H", "3H", "5D" },
-            {  "FREE", "AD", "KD", "QD", "10D", "9D", "8D", "7D", "6D", "FREE" }
-            };
-            return cardArray;
-        }
+        { "FREE", "2S", "3S", "4S", "5S", "6S", "7S", "8S", "9S", "FREE" },
+        { "6C", "5C", "4C", "3C", "2C", "AH", "KH", "QH", "10H", "10S" },
+        { "7C", "AS", "2D", "3D", "4D", "5D", "6D", "7D", "9H", "QS" },
+        { "8C", "KS", "6C", "5C", "4C", "3C", "2C", "8D", "8H", "KS" },
+        { "9C", "QS", "7C", "6H", "5H", "4H", "AH", "9D", "7H", "AS" },
+        { "10C", "10S", "8C", "7H", "2H", "3H", "KH", "10D", "6H", "2D" },
+        { "QC", "9S", "9C", "8H", "9H", "10H", "QH", "QD", "5H", "3D" },
+        { "KC", "8S", "10C", "QC", "KC", "AC", "AD", "KD", "4H", "4D" },
+        {  "AC", "7S", "6S", "5S", "4S", "3S", "2S", "2H", "3H", "5D" },
+        {  "FREE", "AD", "KD", "QD", "10D", "9D", "8D", "7D", "6D", "FREE" }
+        };
+        return cardArray;
     }
 }
